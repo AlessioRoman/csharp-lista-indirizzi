231d7142ac723222371545bb4dab0660f00b46de
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-26 18:09:11 +0200

    added try and catch

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,28 +1,45 @@
 ﻿using address;
 
 List<Address> addresses = new();
-StreamReader file = File.OpenText("C:\\Users\\Utente\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\addresses.csv");
-int lineCounter = 0;
-
-while (!file.EndOfStream)
+try
 {
-    string currentLine = file.ReadLine();
-    lineCounter++;
-    
-    if (lineCounter > 1 && currentLine != null)
+    StreamReader file = File.OpenText("C:\\Users\\Utente\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\addresses.csv");
+    int lineCounter = 0;
+
+    while (!file.EndOfStream)
     {
-        string[] processedInfo = currentLine.Split(',');
-        if (processedInfo.Length != 6) 
+        string currentLine = file.ReadLine();
+        lineCounter++;
+
+        if (lineCounter > 1 && currentLine != null)
         {
-            Console.WriteLine("La riga " + lineCounter + " non rispetta lo standard!");
-        } else
-        {
-            int zipCode = int.Parse(processedInfo[5]);
-            Address newAddress = new(processedInfo[0], processedInfo[1], processedInfo[2], processedInfo[3], processedInfo[4], zipCode);
-            addresses.Add(newAddress);
+            string[] processedInfo = currentLine.Split(',');
+            if (processedInfo.Length != 6)
+            {
+                Console.WriteLine("La riga " + lineCounter + " non rispetta lo standard!");
+            }
+            else
+            {
+                try
+                {
+                    int zipCode = int.Parse(processedInfo[5]);
+                    Address newAddress = new(processedInfo[0], processedInfo[1], processedInfo[2], processedInfo[3], processedInfo[4], zipCode);
+                    addresses.Add(newAddress);
+                } catch (Exception e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+
+            }
         }
     }
 }
+catch (Exception e)
+{
+    Console.WriteLine($"Error: {e.Message}");
+}
+
+
 
 foreach (Address address in addresses)
 {
