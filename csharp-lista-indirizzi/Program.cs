06dd2c2a219ee4ca2af73e1ae440cd37c15bd3fd
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-26 18:02:06 +0200

    Added program.cs logic

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using address;
+
+List<Address> addresses = new();
+StreamReader file = File.OpenText("C:\\Users\\Utente\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\addresses.csv");
+int lineCounter = 0;
+
+while (!file.EndOfStream)
+{
+    string currentLine = file.ReadLine();
+    lineCounter++;
+    
+    if (lineCounter > 1 && currentLine != null)
+    {
+        string[] processedInfo = currentLine.Split(',');
+        if (processedInfo.Length != 6) 
+        {
+            Console.WriteLine("La riga " + lineCounter + " non rispetta lo standard!");
+        } else
+        {
+            int zipCode = int.Parse(processedInfo[5]);
+            Address newAddress = new(processedInfo[0], processedInfo[1], processedInfo[2], processedInfo[3], processedInfo[4], zipCode);
+            addresses.Add(newAddress);
+        }
+    }
+}
+
+foreach (Address address in addresses)
+{
+    Console.WriteLine(address.ToString());
+}
